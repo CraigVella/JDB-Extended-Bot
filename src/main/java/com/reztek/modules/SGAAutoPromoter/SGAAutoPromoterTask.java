ae329181961d75431ab8ad45de9d084e3ab062f5
Chase <cvella@malverne.k12.ny.us>
2017-04-27 14:32:38 -0400

    Revert "Revert "Promotions are now Pretty""
    This reverts commit 8168b7ae1d69476ee999d1282c8d3c1ef649fc2a.

--- src/main/java/com/reztek/modules/SGAAutoPromoter/SGAAutoPromoterTask.java
+++ src/main/java/com/reztek/modules/SGAAutoPromoter/SGAAutoPromoterTask.java
@@ -1,5 +1,7 @@
 package com.reztek.modules.SGAAutoPromoter;
 
+import java.net.URLEncoder;
+
 import com.reztek.base.Taskable;
 import com.reztek.modules.GuardianControl.Guardian;
 import com.reztek.modules.SGAAutoPromoter.SGARankDefines.SGARank;
@@ -37,9 +39,8 @@ public class SGAAutoPromoterTask extends Taskable {
 					if (SGARankDefines.ShouldUpgradeToRank(m, rank)) {
 						getSGAAutoPromoterCommands().getSGAGuild().getController().addRolesToMember(m, getSGAAutoPromoterCommands().getSGAGuild().getRoleById(rank.getRankId())).queue();
 						EmbedBuilder eb = new EmbedBuilder();
-						eb.setDescription(m.getAsMention() + "\n```markdown\n[ATTENTION][ATTENTION][ATTENTION][ATTENTION][ATTENTION]\n\n"
-								        + g.getName() + " HAS PROGRESSED TO RANK (" + rank.getRankTitle() + ")"
-										+ "\n\n[ATTENTION][ATTENTION][ATTENTION][ATTENTION][ATTENTION]```");
+						eb.setDescription(m.getAsMention());
+						eb.setImage("http://reztek.net/SGA/SGAFunctions.php?u=" + URLEncoder.encode(g.getName()) + "&r=" + URLEncoder.encode(rank.getRankTitle()));
 						eb.setColor(getSGAAutoPromoterCommands().getSGAGuild().getRoleById(rank.getRankId()).getColor());
 						eb.setFooter("Congratulations Guardian", "https://s-media-cache-ak0.pinimg.com/736x/15/fc/63/15fc63d39f85b5c73b286a58781645ae.jpg");
 						getSGAAutoPromoterCommands().getSGACourtyardChannel().sendMessage(eb.build()).queue();
